90116d146922ccada01de919609329343f45368b
aru-ms <104093401+aru-ms@users.noreply.github.com>
2023-06-05 13:03:36 +0200

    SignUp en Compose funcionando

--- app/src/main/java/com/alberto/recipegram/SignUpActivity.kt
+++ app/src/main/java/com/alberto/recipegram/SignUpActivity.kt
@@ -2,102 +2,154 @@ package com.alberto.recipegram
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alberto.recipegram.ui.theme.RSRecetasTheme
-import com.alberto.recipegram.viewmodel.SignUpViewModel
-import com.google.firebase.ktx.Firebase
-import dagger.hilt.android.AndroidEntryPoint
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-@AndroidEntryPoint
 class SignUpActivity : ComponentActivity() {
-
-    private val viewModel: SignUpViewModel by viewModels()
+    private lateinit var auth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+    private val progressBarVisible = mutableStateOf(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().reference.child("Usuarios")
 
         setContent {
-
+            RSRecetasTheme() {
+                SignUpScreen()
+            }
         }
+    }
 
-        @Composable
-        fun SignUpScreen() {
-            val progressBarVisible = remember { mutableStateOf(false) }
+    @Composable
+    private fun SignUpScreen() {
+        val firstName = remember { mutableStateOf("") }
+        val lastName = remember { mutableStateOf("") }
+        val email = remember { mutableStateOf("") }
+        val password = remember { mutableStateOf("") }
+        val confirmPassword = remember { mutableStateOf("") }
 
-            val firstName by remember { viewModel.firstName }
-            val lastName by remember { viewModel.lastName }
-            val email by remember { viewModel.email }
-            val password by remember { viewModel.password }
-            val confirmPassword by remember { viewModel.confirmPassword }
 
+        Surface(modifier = Modifier.fillMaxSize()) {
             Column(
-                modifier = Modifier.padding(16.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                TextField(
-                    value = firstName,
-                    onValueChange = { viewModel.setFirstName(it) },
-                    label = { Text("Nombre") }
+                OutlinedTextField(
+                    value = firstName.value,
+                    onValueChange = { firstName.value = it },
+                    label = { Text("Nombre") },
+                    modifier = Modifier.fillMaxWidth()
                 )
-                TextField(
-                    value = lastName,
-                    onValueChange = { viewModel.setLastName(it) },
+                OutlinedTextField(
+                    value = lastName.value,
+                    onValueChange = { lastName.value = it },
                     label = { Text("Apellidos") },
-
+                    modifier = Modifier.fillMaxWidth()
                 )
-                TextField(
-                    value = email,
-                    onValueChange = { viewModel.setEmail(it) },
+                OutlinedTextField(
+                    value = email.value,
+                    onValueChange = { email.value = it },
                     label = { Text("Email") },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Email,
-                        imeAction = ImeAction.Next
-                    )
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
                 )
-                TextField(
-                    value = password,
-                    onValueChange = { viewModel.setPassword(it) },
-                    label = { Text("Password") },
+                OutlinedTextField(
+                    value = password.value,
+                    onValueChange = { password.value = it },
+                    label = { Text("Contraseña") },
+                    modifier = Modifier.fillMaxWidth(),
                     visualTransformation = PasswordVisualTransformation()
                 )
-                TextField(
-                    value = confirmPassword,
-                    onValueChange = { viewModel.setConfirmPassword(it) },
-                    label = { Text("Confirm Password") },
+                OutlinedTextField(
+                    value = confirmPassword.value,
+                    onValueChange = { confirmPassword.value = it },
+                    label = { Text("Confirma tu contraseña") },
+                    modifier = Modifier.fillMaxWidth(),
                     visualTransformation = PasswordVisualTransformation()
                 )
                 Button(
-                    onClick = { viewModel.createNewAccount() },
+                    onClick = { createNewAccount(firstName.value, lastName.value, email.value, password.value, confirmPassword.value) },
                     modifier = Modifier.align(Alignment.End)
                 ) {
-                    Text("Sign Up")
+                    Text("Regístrate")
                 }
                 if (progressBarVisible.value) {
                     CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
                 }
             }
         }
+    }
+
+    private fun createNewAccount(firstName: String, lastName: String, email: String, password: String, confirmPassword: String) {
+        if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && password == confirmPassword) {
+            progressBarVisible.value = true
+
+            auth.signOut()
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        user?.sendEmailVerification()?.addOnCompleteListener { verificationTask ->
+                            if (verificationTask.isSuccessful) {
+                                showToast("Verify your email with the received link")
+                                goMain()
+                            } else {
+                                showToast("Error verifying email")
+                            }
+                        }
+                    } else {
+                        showToast("Error creating user")
+                    }
+                    progressBarVisible.value = false
+                }
+        } else {
+            showToast("Fill in all the fields or the passwords do not match")
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun goMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        finish()
     }
 }
