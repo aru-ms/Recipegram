a16fb5c2a79a0bece2e202cd0de263081ff26c56
Alberto <vhiek.ams@gmail.com>
2023-06-06 13:49:06 +0200

    Separar los fragments en clases Mover Recipe a paquete modelo

---
+++ app/src/main/java/com/alberto/recipegram/SearchFragment.kt
@@ -0,0 +1,44 @@
+package com.alberto.recipegram
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchFragment() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Home Screen",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primary
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                // Handle button click
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Search Screen")
+        }
+    }
+}
--- app/src/main/java/com/alberto/recipegram/model/Recipe.kt
+++ app/src/main/java/com/alberto/recipegram/model/Recipe.kt
@@ -1,4 +1,4 @@
-package com.alberto.recipegram
+package com.alberto.recipegram.model
 
 data class Recipe(
     var title: String?= "",
